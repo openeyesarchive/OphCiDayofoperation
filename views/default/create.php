1fe61d4ee8c077b9342f8e524c6e3627665fa43c
Adam Dyer <progen91@gmail.com>
2013-11-07 11:52:14 +0000

    [OE-3723] day of operation updates

--- views/default/create.php
+++ views/default/create.php
@@ -1,22 +1,21 @@
-<?php 	$this->breadcrumbs=array($this->module->id);
-	$this->header();
+<?php $this->breadcrumbs=array($this->module->id);?>
+<?php $this->beginContent('//patient/event_container'); ?>
+
+<h2 class="event-title"><?php echo $this->event_type->name ?></h2>
+
+<?php
+$form = $this->beginWidget('BaseEventTypeCActiveForm', array(
+		'id'=>'clinical-create',
+		'enableAjaxValidation'=>false,
+		'htmlOptions' => array('class'=>'sliding'),
+		'layoutColumns'=>array('label'=>4,'field'=>8),
+	));
+$this->event_actions[] = EventAction::button('Save', 'save', array('level' => 'secondary'), array('class' => 'button small'));
 ?>
-<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php echo $this->event_type->name ?></h3>
 
+<?php  $this->displayErrors($errors)?>
+<?php  $this->renderDefaultElements($this->action->id, $form); ?>	<?php  $this->renderOptionalElements($this->action->id, $form); ?>
+<?php  $this->displayErrors($errors)?>
+<?php  $this->endWidget(); ?>
 
-<div>
-	<?php 		$form = $this->beginWidget('BaseEventTypeCActiveForm', array(
-			'id'=>'clinical-create',
-			'enableAjaxValidation'=>false,
-			'htmlOptions' => array('class'=>'sliding'),
-			// 'focus'=>'#procedure_id'
-		));
-    $this->event_actions[] = EventAction::button('Save', 'save', array('colour' => 'green'));
-    $this->renderPartial('//patient/event_actions');
-	?>
-	<?php  $this->displayErrors($errors)?>
-	<?php  $this->renderDefaultElements($this->action->id, $form); ?>	<?php  $this->renderOptionalElements($this->action->id, $form); ?>
-	<?php  $this->displayErrors($errors)?>
-	<?php  $this->endWidget(); ?></div>
-
-<?php  $this->footer(); ?>
+<?php $this->endContent() ;?>
--- views/default/form_Element_OphCiDayofoperation_Details.php
+++ views/default/form_Element_OphCiDayofoperation_Details.php
@@ -15,32 +15,48 @@
  * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
- ?>
-<div class="element <?php echo $element->elementType->class_name?>"
-	data-element-type-id="<?php echo $element->elementType->id ?>"
-	data-element-type-class="<?php echo $element->elementType->class_name ?>"
-	data-element-type-name="<?php echo $element->elementType->name ?>"
-	data-element-display-order="<?php echo $element->elementType->display_order ?>">
-	<h4 class="elementTypeName"><?php  echo $element->elementType->name; ?></h4>
+?>
+<section class="element <?php echo $element->elementType->class_name?>"
+		 data-element-type-id="<?php echo $element->elementType->id ?>"
+		 data-element-type-class="<?php echo $element->elementType->class_name ?>"
+		 data-element-type-name="<?php echo $element->elementType->name ?>"
+		 data-element-display-order="<?php echo $element->elementType->display_order ?>">
+	<h3><?php  echo $element->elementType->name; ?></h3>
 
-    <div class="eventDetail" style="padding-left: 13.9em;">
-        <?php echo CHtml::encode($element->getAttributeLabel('medical_history_changed'))?>
+	<div class="element-fields">
+		<div class="row">
+			<div class="large-4 column">
+				<label><?php echo CHtml::encode($element->getAttributeLabel('medical_history_changed'))?>:</label>
+			</div>
+			<div class="large-8 column">
+				<?php echo $form->radioBoolean($element, 'medical_history_changed', array('nowrapper'=>1))?>
+				<?php $showchange = $_POST ? $_POST["Element_OphCiDayofoperation_Details"]["medical_history_changed"] : $element->medical_history_changed;?>
+				<div class="changelabel"<?php if(!$showchange) echo ' style="display:none"'; else echo ' style="display:inline-block"'?>>
+					<?php echo CHtml::encode($element->getAttributeLabel('change'))?>
+					<?php echo $form->textField($element, 'change',array('nowrapper'=>1))?>
+				</div>
+			</div>
+		</div>
+
+		<?php echo $form->textField($element, 'inr_level', array('size' => '10')); ?>
+
+		<div class="element-fields">
+			<div class="row">
+				<div class="large-4 column">
+				</div>
+				<div class="large-8 column">
+					<?php echo $form->checkBox($element, 'preop_checklist', array('nowrapper'=>true))?>
+				</div>
+			</div>
+		</div>
+		<div class="element-fields">
+			<div class="row">
+				<div class="large-4 column">
+				</div>
+				<div class="large-8 column">
+					<?php echo $form->checkBox($element, 'cjd_checklist', array('nowrapper'=>true))?>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="eventDetail" style="padding-left: 13.9em;">
-		<?php echo $form->radioBoolean($element, 'medical_history_changed', array('nowrapper'=>1))?>
-        <?php $showchange = $_POST ? $_POST["Element_OphCiDayofoperation_Details"]["medical_history_changed"] : $element->medical_history_changed;?>
-        <div class="changelabel"<?php if(!$showchange) echo ' style="display:none"'; else echo ' style="display:inline-block"'?>>
-            <?php echo CHtml::encode($element->getAttributeLabel('change'))?>
-	        <?php echo $form->textField($element, 'change',array('nowrapper'=>1))?>
-        </div>
-	</div>
-	<?php echo $form->textField($element, 'inr_level', array('size' => '10')); ?>
-	<div class="eventDetail" style="padding-left: 13.9em;">
-		<?php echo $form->checkBox($element, 'preop_checklist', array('nowrapper'=>true))?>
-		<?php echo CHtml::encode($element->getAttributeLabel('preop_checklist'))?>
-	</div>
-	<div class="eventDetail" style="padding-left: 13.9em;">
-		<?php echo $form->checkBox($element, 'cjd_checklist', array('nowrapper'=>true))?>
-		<?php echo CHtml::encode($element->getAttributeLabel('cjd_checklist'))?>
-	</div>
-</div>
+</section>
