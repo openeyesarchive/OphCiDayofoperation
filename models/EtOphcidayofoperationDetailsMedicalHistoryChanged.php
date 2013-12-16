3b59120a577e059ae9fcabcdbfdf98cc0783ee8a
Mark <m@rkw.me>
2013-12-16 15:13:14 +0000

    [OE-3856] changed model inheritance to use the new versioned base model

--- models/EtOphcidayofoperationDetailsMedicalHistoryChanged.php
+++ models/EtOphcidayofoperationDetailsMedicalHistoryChanged.php
@@ -26,7 +26,7 @@
  * The followings are the available model relations:
  */
 
-class EtOphcidayofoperationDetailsMedicalHistoryChanged extends BaseActiveRecord
+class EtOphcidayofoperationDetailsMedicalHistoryChanged extends BaseActiveRecordVersioned
 {
 	/**
 	 * Returns the static model of the specified AR class.
